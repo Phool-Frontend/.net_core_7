4cafcc0ccae5b499adf773d5c884ff1d0c9c3ac4
phool herrera condezo <pherrerac@bcds.net>
2023-11-25 16:03:23 -0500

    [Add] - video 34 done

---
+++ Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ga.Data;
+using ga.Models.Country;
+
+namespace ga.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Country, CreateCountryDto>().ReverseMap();
+        }
+    }
+}
