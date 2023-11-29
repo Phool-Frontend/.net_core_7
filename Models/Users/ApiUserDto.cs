f75766477841ff3439816c13028543734f5fc2bc
phool herrera condezo <pherrerac@bcds.net>
2023-11-28 22:43:02 -0500

    [Add] - video 54 done

--- Models/Users/ApiUserDto.cs
+++ Models/Users/ApiUserDto.cs
@@ -2,20 +2,13 @@
 
 namespace ga.Models.Users
 {
-    public class ApiUserDto
+    public class ApiUserDto : LoginDto
     {
         [Required]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        [EmailAddress]
-        public string Email { get; set; }
-        [Required]
-        [StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters",MinimumLength = 6)]
-        public string Password { get; set; }
-
+        
     }
 
 
